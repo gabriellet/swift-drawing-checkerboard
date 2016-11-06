c18aa5df584e34a8e19a7872e918ae69f2923edd
Gabrielle <gat2118@columbia.edu>
2016-11-06 13:20:07 -0500

    First Playgrounds

---
+++ CoreGraphicsExperiments.playground/Contents.swift
@@ -0,0 +1,74 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+class GradientView : UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+        guard let context: CGContext = UIGraphicsGetCurrentContext() else { return }
+        
+        let dotBlue = UIColor(red: 39.0/255.0, green: 119.0/255.0, blue: 174.0/255.0, alpha: 1.0).cgColor
+        let lineBlue = UIColor(red: 35.0/255.0, green: 80.0/255.0, blue: 145.0/255.0, alpha: 0.2).cgColor
+        let startBlue = UIColor(red: 27.0/255.0, green: 94.0/255.0, blue: 125.0/255.0, alpha: 1.0).cgColor
+        let endBlue = UIColor(red: 44.0/255.0, green: 60.0/255.0, blue: 110.0/255.0, alpha: 1.0).cgColor
+        
+        let locations: [CGFloat] = [0.0, 0.75]
+        
+        let colors = [startBlue, endBlue]
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let gradient: CGGradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: locations)!
+        
+        let startPoint: CGPoint = CGPoint(x: 0.0, y: 0.0)
+        let endPoint: CGPoint = CGPoint(x: rect.size.width, y: rect.size.height)
+        
+        context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: CGGradientDrawingOptions(rawValue: 0))
+        
+        let diam: CGFloat = 6.0
+        let radius: CGFloat = diam / 2.0
+        let step: CGFloat = 60.0
+        let startX: CGFloat = (rect.size.width.truncatingRemainder(dividingBy: step)) * 0.5
+        let startY: CGFloat = (rect.size.height.truncatingRemainder(dividingBy: step)) * 0.5
+        
+        context.setStrokeColor(lineBlue)
+        context.setLineWidth(2.0)
+        
+        for i in stride(from: startX, to: rect.size.width, by: 2.0 * step) {
+            context.move(to: CGPoint(x: i, y: 0.0))
+            context.addLine(to: CGPoint(x: i, y: rect.size.height))
+            context.setShadow(offset: CGSize(width: 0, height: 0), blur: 4.0, color: lineBlue)
+        }
+        
+        for j in stride(from: startX, to: rect.size.height, by: 2.0 * step) {
+            context.move(to: CGPoint(x: 0.0, y: j))
+            context.addLine(to: CGPoint(x: rect.size.width, y: j))
+            context.setShadow(offset: CGSize(width: 0, height: 0), blur: 4.0, color: lineBlue)
+        }
+        
+        context.strokePath()
+        
+        for i in stride(from: startX, to: rect.size.width, by: step) {
+            
+            for j in stride(from: startY, to: rect.size.height, by: step) {
+                
+                let point = CGPoint(x: rect.origin.x + i, y: rect.origin.y + j)
+                context.setShadow(offset: CGSize(width: 0, height: 0), blur: 4.0, color: dotBlue)
+                context.setFillColor(dotBlue)
+                let circle = CGRect(x: point.x - radius, y: point.y - radius, width: diam, height: diam)
+                context.addEllipse(in: circle)
+                
+            }
+            
+        }
+        
+        context.fillPath()
+        
+    }
+    
+}
+
+var baseView = GradientView(frame: CGRect(x: 0.0, y: 0.0, width: 1024.0, height: 768.0))
+//PlaygroundPage.current.liveView = baseView
